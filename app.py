c5e5ea669f5bd7c2a9b8f3ae60871fd953e64492
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2023-12-13 14:54:30 -0500

    ready for game

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+game_status = "pending"
+print(game_status )
 
 @app.route('/')
 def index():
-    return 'Hello, this is a basic Flask server!'
+    
+    return render_template('index.html', game_status=game_status)
+
+@app.route('/update_status', methods=['POST'])
+def update_status():
+    global game_status
+    # Change the game status here
+    game_status = "started" if game_status == "pending" else "pending"
+    print(game_status)
+    return redirect(url_for('index')) 
 
 if __name__ == '__main__':
     app.run(debug=True)
